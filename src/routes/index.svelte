749a2ed7c066c2917fc9724b40b94cc6875015c0
battlegamez <ultralanpasalan0@gmail.com>
2021-12-19 04:10:50 +0000

    eeeee

---
+++ src/routes/index.svelte
@@ -0,0 +1,22 @@
+<script>
+    let name = ""
+    let gender = ""
+
+    
+    async function getName() {
+        fetch('https://api.genderize.io/?name=' + name)
+        .then((response) => response.json())
+        .then((data) => {
+          gender = data.gender; 
+        });
+    }
+    </script>
+    
+    
+    <h1>Weather App</h1>
+    <p>Gender: {gender}</p>
+    <form on:submit|preventDefault={getName}>
+        <label for="name">Name:</label>
+        <input id="name" bind:value={name} type="text" />
+        <button type="submit">Check</button>
+    </form>
